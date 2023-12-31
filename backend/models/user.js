de98df5f506f1b253a136f0c389f49bc688318a9
Shailesh pandey <pandeyshailesh2081998@gmail.com>
2023-12-31 12:15:46 +0530

    create  my first schema in Models folder

---
+++ backend/models/user.js
@@ -0,0 +1,35 @@
+// this is a Schema of User (I called it as userSchema)
+
+const mongoose = required("mongoose");
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 40,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        maxlength: 40,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        maxlength: 40,
+        trim: true
+
+    },
+    mobile: {
+        type: Number,
+        required: true,
+        maxlength: 10,
+        trim: true
+    }
+
+});
+
+module.exports = mongoose.model("User", userSchema);
