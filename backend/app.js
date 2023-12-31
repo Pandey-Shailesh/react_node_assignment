d50c7e5abb223b26c50b770ac79da673e9fe8728
Shailesh pandey <pandeyshailesh2081998@gmail.com>
2023-12-31 11:53:28 +0530

    Connect with Data Base (MongoDB)

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,34 @@
-const express= require("express");
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
-const port=8000;
-app.get("/",(req,res)=>{
-    res.send("Hello server");
+
+//DB Connection
+mongoose.connect(process.env.DATABASE)
+    .then(() => console.log('MongoDB Database Connected!'));
+
+//Middlewares
+app.use(bodyParser.json());
+app
+
+
+
+.use(cookieParser());
+app.use(cors());
+
+
+//PORT
+const port = process.env.PORT || 8000;
+// app.get("/", (req, res) => {
+//     res.send("Hello server");
+// });
+
+//Starting a server
+app.listen(port, () => {
+    console.log(`server is up and Running at Port ${port}`);
 });
-app.listen(port, ()=>{
-    console.log("server is up and Running");
-});
+
